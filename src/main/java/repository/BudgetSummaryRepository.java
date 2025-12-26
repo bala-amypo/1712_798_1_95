d2ba071d601c71bbbc628344e98b4e1e934ee9a4
bala-amypo <bala@amypo.in>
2025-12-26 16:43:18 +0000

    User commit

--- src/main/java/repository/BudgetSummaryRepository.java
+++ src/main/java/repository/BudgetSummaryRepository.java
@@ -4,10 +4,14 @@ import com.example.demo.model.BudgetPlan;
 import com.example.demo.model.BudgetSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
+
+@Repository
 
 public interface BudgetSummaryRepository
         extends JpaRepository<BudgetSummary, Long> {
 
-    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan budgetPlan);
+    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan plan);
 }
--- src/main/java/repository/TransactionLogRepository.java
+++ src/main/java/repository/TransactionLogRepository.java
@@ -4,8 +4,12 @@ import com.example.demo.model.TransactionLog;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.util.List;
+
+@Repository
 
 public interface TransactionLogRepository
         extends JpaRepository<TransactionLog, Long> {
